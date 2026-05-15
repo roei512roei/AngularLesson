706b6f79c504eacef22caf67282e282aa9de98f9
Roei Avrahami <roei512roei@Gmail.com>
2026-05-15 23:11:52 +0300

    Added imports and did ngfor loop for displaying products

--- my-app/src/app/features/products/product-list/product-list.ts
+++ my-app/src/app/features/products/product-list/product-list.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
-interface Product{
+import { CommonModule } from '@angular/common';
+
+interface Product {
   name: string;
   price: number;
   category: string;
-
 }
+
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './product-list.html',
-  styleUrl: './product-list.css',
+  styleUrls: ['./product-list.css']
 })
 export class ProductList {
 
   products: Product[] = [
-    {name:"Chair",price:100,category:"Decor"},
-    {name:"Table",price:400,category:"Decor"},
-    {name:"TV",price:1500,category:"Electronics"},
-    {name:"Chandelier",price:800,category:"Decor"},
-    {name:"Washing Machine",price:1000,category:"Electronics"},
-  ]
+    { name: "Chair", price: 100, category: "Decor" },
+    { name: "Table", price: 400, category: "Decor" },
+    { name: "TV", price: 1500, category: "Electronics" },
+    { name: "Chandelier", price: 800, category: "Decor" },
+    { name: "Washing Machine", price: 1000, category: "Electronics" },
+  ];
+
 }
