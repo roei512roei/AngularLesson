a506798ddb32742296fe97eb2e0fbf7c392ad50a
Roei Avrahami <roei512roei@Gmail.com>
2026-05-16 15:38:57 +0300

    Added products route to website

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { ProductList } from './features/products/product-list/product-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: '', redirectTo: 'products', pathMatch: 'full'},
+    { path: 'products', component: ProductList },];
