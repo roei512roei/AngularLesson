7120181c0ab1f05d0e90cb5ab8a6dbe51453799a
Roei Avrahami <roei512roei@Gmail.com>
2026-05-16 16:07:26 +0300

    Added routes for every new component

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -2,4 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductList } from './features/products/product-list/product-list';
 
 export const routes: Routes = [{path: '', redirectTo: 'products', pathMatch: 'full'},
-    { path: 'products', component: ProductList },];
+    { path: 'products', component: ProductList },
+    { path: 'orders', component: OrderList },
+    { path: 'dashboard', component: DashboardPage },
+    { path: 'productdetails', component: ProductDetails }];
