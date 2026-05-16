d249a4c2c68211e372fe5e02465cd4d5d2ffa1a8
Roei Avrahami <roei512roei@Gmail.com>
2026-05-16 16:01:31 +0300

    Created components

---
+++ my-app/src/app/features/products/order-component/order-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-component',
+  imports: [],
+  templateUrl: './order-component.html',
+  styleUrl: './order-component.css',
+})
+export class OrderComponent {}
---
+++ my-app/src/app/features/products/product-details-component/product-details-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details-component',
+  imports: [],
+  templateUrl: './product-details-component.html',
+  styleUrl: './product-details-component.css',
+})
+export class ProductDetailsComponent {}
